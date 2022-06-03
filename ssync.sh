7215b0f73630ed21ed2a059b5132b39cba7ca137
Gvenetadze Vakhtang <vakhtang.st@gmail.com>
2022-06-03 13:13:25 +0300

    Version detection method changed

--- ssync.sh
+++ ssync.sh
@@ -6,21 +6,16 @@ SCRIPT_PATH=$(dirname "$0")
 
 . "${SCRIPT_PATH}"/../.ssync/ssync.conf
 
-if [ ! -f ${LOCAL_PATH}/${TIME_STATE_FILE} ]
+if [ ! -f ${LOCAL_PATH}/${VERSION} ]
 then
-    mkdir -p `dirname ${LOCAL_PATH}/${TIME_STATE_FILE}`
-    echo 0 > ${LOCAL_PATH}/${TIME_STATE_FILE}
+    mkdir -p `dirname ${LOCAL_PATH}/${VERSION}`
+    echo 0 > ${LOCAL_PATH}/${VERSION}
 fi
 
-if [ ! -f ${LOCAL_PATH}/${SYNC_STATE_FILE} ]
-then
-    mkdir -p `dirname ${LOCAL_PATH}/${SYNC_STATE_FILE}`
-    touch ${LOCAL_PATH}/${SYNC_STATE_FILE}
-fi
 
 if [ ! -f ${LOCAL_PATH}/${EXCLUDE} ]
 then
-    mkdir -p `dirname ${LOCAL_PATH}/${TIME_STATE_FILE}`
+    mkdir -p `dirname ${LOCAL_PATH}/${VERSION}`
     touch ${LOCAL_PATH}/${EXCLUDE}
 fi
 
@@ -32,32 +27,24 @@ fi
 
 echo "------------------------ Start ------------------------" >> ${LOG_FILE}
 
-local_state_version=`cat ${LOCAL_PATH}/${TIME_STATE_FILE}`
-
-rm -f ${LOCAL_PATH}/${SYNC_STATE_FILE}
-rsync -e "ssh -p ${SSH_PORT}" --log-file=${LOG_FILE} ${SERVER_PATH}/.ssync/*.state ${LOCAL_PATH}/.ssync/ >/dev/null 2>&1
-
-if [[ -f "${LOCAL_PATH}/${SYNC_STATE_FILE}" && `cat ${LOCAL_PATH}/${SYNC_STATE_FILE}` != "done" ]]
+local_state_version=`cat ${LOCAL_PATH}/${VERSION}`
+rm -f /tmp/ssync_version.state
+rsync -e "ssh -p ${SSH_PORT}" --log-file=${LOG_FILE} ${SERVER_PATH}/${VERSION} /tmp/ssync_version.state >/dev/null 2>&1
+if [ ! -f /tmp/ssync_version.state ]
 then
-    echo "Synced not comlited"
-    exit 0
+    server_state_version=0
+else
+    server_state_version=`cat /tmp/ssync_version.state`
 fi
 
-echo "progress" > ${LOCAL_PATH}/${SYNC_STATE_FILE}
-
-if [ ! -f ${LOCAL_PATH}/${TIME_STATE_FILE} ] || (( `cat ${LOCAL_PATH}/${TIME_STATE_FILE}` <= $local_state_version ))
+if (( $local_state_version >= $server_state_version ))
 then
     echo "LOCAL =========> SERVER" | tee -a ${LOG_FILE}
-    echo $(date +%s) > ${LOCAL_PATH}/${TIME_STATE_FILE}    
-    rsync -e "ssh -p ${SSH_PORT}" -au --relative --delete --log-file=${LOG_FILE} ${LOCAL_PATH}/./${SYNC_STATE_FILE} ${SERVER_PATH}/
-
+    echo $(date +%s) > ${LOCAL_PATH}/${VERSION}
     rsync -e "ssh -p ${SSH_PORT}" -au --delete --exclude-from=${LOCAL_PATH}/${EXCLUDE} --log-file=${LOG_FILE} ${LOCAL_PATH}/ ${SERVER_PATH}
-    
-    echo "done" > ${LOCAL_PATH}/${SYNC_STATE_FILE}
-    rsync -e "ssh -p ${SSH_PORT}" -au --relative --delete --log-file=${LOG_FILE} ${LOCAL_PATH}/./${SYNC_STATE_FILE} ${SERVER_PATH}/
 else
     echo "SERVER =========> LOCAL" | tee -a ${LOG_FILE}
+    echo ${server_state_version} > ${LOCAL_PATH}/${VERSION}
     rsync -e "ssh -p ${SSH_PORT}" -au --delete --exclude-from=${LOCAL_PATH}/${EXCLUDE} --log-file=${LOG_FILE} ${SERVER_PATH}/ ${LOCAL_PATH}
 fi
-
 echo "------------------------  END  ------------------------" >> ${LOG_FILE}
