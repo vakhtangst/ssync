5e1cda0d33ee0ef39e774adbe3af69093795c182
Gvenetadze Vakhtang <vakhtang.st@gmail.com>
2022-06-03 15:23:32 +0300

    Version detection method changed

--- ssync.sh
+++ ssync.sh
@@ -28,23 +28,22 @@ fi
 echo "------------------------ Start ------------------------" >> ${LOG_FILE}
 
 local_state_version=`cat ${LOCAL_PATH}/${VERSION}`
-rm -f /tmp/ssync_version.state
-rsync -e "ssh -p ${SSH_PORT}" --log-file=${LOG_FILE} ${SERVER_PATH}/${VERSION} /tmp/ssync_version.state >/dev/null 2>&1
-if [ ! -f /tmp/ssync_version.state ]
+
+if [ $(ssh ${SSH_CONNECT} "[[ -f ${SERVER_PATH}/.ssync/version.state ]]"; echo $?) -eq 0 ]
 then
-    server_state_version=0
+    server_state_version=$(ssh ${SSH_CONNECT} "cat ${SERVER_PATH}/.ssync/version.state")
 else
-    server_state_version=`cat /tmp/ssync_version.state`
+    server_state_version=0
 fi
 
 if (( $local_state_version >= $server_state_version ))
 then
     echo "LOCAL =========> SERVER" | tee -a ${LOG_FILE}
     echo $(date +%s) > ${LOCAL_PATH}/${VERSION}
-    rsync -e "ssh -p ${SSH_PORT}" -au --delete --exclude-from=${LOCAL_PATH}/${EXCLUDE} --log-file=${LOG_FILE} ${LOCAL_PATH}/ ${SERVER_PATH}
+    rsync -e "ssh -p ${SSH_PORT}" -au --delete --exclude-from=${LOCAL_PATH}/${EXCLUDE} --log-file=${LOG_FILE} ${LOCAL_PATH}/ ${SSH_CONNECT}:${SERVER_PATH}
 else
     echo "SERVER =========> LOCAL" | tee -a ${LOG_FILE}
     echo ${server_state_version} > ${LOCAL_PATH}/${VERSION}
-    rsync -e "ssh -p ${SSH_PORT}" -au --delete --exclude-from=${LOCAL_PATH}/${EXCLUDE} --log-file=${LOG_FILE} ${SERVER_PATH}/ ${LOCAL_PATH}
+    rsync -e "ssh -p ${SSH_PORT}" -au --delete --exclude-from=${LOCAL_PATH}/${EXCLUDE} --log-file=${LOG_FILE} ${SSH_CONNECT}:${SERVER_PATH}/ ${LOCAL_PATH}
 fi
 echo "------------------------  END  ------------------------" >> ${LOG_FILE}
