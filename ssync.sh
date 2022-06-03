13d1bca01cb1d3a35ce62fea66e61987b2653a46
Gvenetadze Vakhtang <vakhtang.st@gmail.com>
2022-06-03 15:45:01 +0300

    chenged logging label

--- ssync.sh
+++ ssync.sh
@@ -38,11 +38,11 @@ fi
 
 if (( $local_state_version >= $server_state_version ))
 then
-    echo "LOCAL =========> SERVER" | tee -a ${LOG_FILE}
+    echo "LOCAL                  =========>                SERVER" | tee -a ${LOG_FILE}
     echo $(date +%s) > ${LOCAL_PATH}/${VERSION}
     rsync -e "ssh -p ${SSH_PORT}" -au --delete --exclude-from=${LOCAL_PATH}/${EXCLUDE} --log-file=${LOG_FILE} ${LOCAL_PATH}/ ${SSH_CONNECT}:${SERVER_PATH}
 else
-    echo "SERVER =========> LOCAL" | tee -a ${LOG_FILE}
+    echo "SERVER                 =========>                 LOCAL" | tee -a ${LOG_FILE}
     echo ${server_state_version} > ${LOCAL_PATH}/${VERSION}
     rsync -e "ssh -p ${SSH_PORT}" -au --delete --exclude-from=${LOCAL_PATH}/${EXCLUDE} --log-file=${LOG_FILE} ${SSH_CONNECT}:${SERVER_PATH}/ ${LOCAL_PATH}
 fi
